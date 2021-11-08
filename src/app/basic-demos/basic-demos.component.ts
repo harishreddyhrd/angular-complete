89583da22e2731933baf06735234f1e7c6bb7ede
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 14:46:16 +0530

    pendingTask added

--- src/app/basic-demos/basic-demos.component.ts
+++ src/app/basic-demos/basic-demos.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-basic-demos',
-  template: `<p>TEMPLATE Works</p>`,
-  // templateUrl: './basic-demos.component.html',
+  // template: `<p>TEMPLATE Works</p>`,
+  templateUrl: './basic-demos.component.html',
   styleUrls: ['./basic-demos.component.scss'],
 })
 export class BasicDemosComponent implements OnInit {
+  toDoVideosList: string[] = [
+    '9. Working with StyleUrls and Styles',
+    '10. toDoVideosList',
+  ];
   constructor() {}
 
   ngOnInit(): void {}
