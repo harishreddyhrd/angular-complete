ef553e6bdea4d7b909f8ec600daa82dd5dd32191
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 14:31:25 +0530

    component-generation using CLI: (1) ng generate component users or (2) ng g c users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyDoubts } from './my-doubts/my-doubts.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, MyDoubts],
+  declarations: [AppComponent, MyDoubts, UsersComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
