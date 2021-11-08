642c84025d0ab32af19c9586359bda89093533f8
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 15:04:08 +0530

    Pending Task added

--- src/app/basic-demos/basic-demos.component.ts
+++ src/app/basic-demos/basic-demos.component.ts
@@ -10,6 +10,7 @@ export class BasicDemosComponent implements OnInit {
   toDoVideosList: string[] = [
     '9. Working with StyleUrls and Styles',
     '10. Understanding Different types of Selectors supported for creating components in angular.',
+    '11. Implement Data Binding in the Angular. Understanding String Interpolation in the Html File.',
   ];
   constructor() {}
 
