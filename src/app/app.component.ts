72dd542d8fb343db5f0a6e7466b55bd8190d210a
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 16:03:34 +0530

    $event - demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,16 @@ export class AppComponent {
   title = 'Event Binding';
   description = `Event binding = <code>appending the data on an event fire</code>`;
   showDescription: boolean = false;
+  givenInput!: string;
 
   toggleDescription() {
     this.showDescription = !this.showDescription;
+  }
+
+  eventHandler(event: Event) {
+    console.log(event);
+    console.log((event.target as HTMLInputElement).value);
+    console.log((<HTMLInputElement>event.target).value);
+    this.givenInput = (<HTMLInputElement>event.target).value;
   }
 }
