01f31ca612b366fd713b886fedc5ed2e81ed7034
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 15:21:33 +0530

    Data binding - demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-complete';
+  title = 'Data Binding';
+  description =
+    'Data binding is nothing but the transfer of information from the local TS file to local HTML template';
 }
