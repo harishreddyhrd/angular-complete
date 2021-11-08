e8f49fb7b404ff8ebde7cbf2dca6c24fa8e50ef2
harishreddyhrd <hari.000703@gmail.com>
2021-11-09 05:11:55 +0530

    ngStyle demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'inititate';
-  description = 'description';
+  title = 'Attribute directives';
+  description = 'ngStyle, ngClass';
+  userStatus: boolean = Math.floor(Math.random() * 10) + 1 > 5 ? true : false;
+
+  getBackgroundColor() {
+    return this.userStatus ? 'lightgreen' : 'lightcoral';
+  }
 }
