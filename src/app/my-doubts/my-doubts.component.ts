379b9e227adcf177043b37a7857a5387a520aa34
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 14:17:06 +0530

    component-generated-manually

---
+++ src/app/my-doubts/my-doubts.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-doubts',
+  templateUrl: './my-doubts.component.html',
+})
+export class MyDoubts {
+  queriesList: string[] = ["What's a @Decorator?"];
+}
