65d4ed7b6118bc4d024d10cff08961bb5ca1a6a7
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 15:12:30 +0530

    Pending Task added

--- src/app/basic-demos/basic-demos.component.ts
+++ src/app/basic-demos/basic-demos.component.ts
@@ -11,11 +11,15 @@ export class BasicDemosComponent implements OnInit {
     '9. Working with StyleUrls and Styles',
     '10. Understanding Different types of Selectors supported for creating components in angular.',
     '11. Implement Data Binding in the Angular. Understanding String Interpolation in the Html File.',
+    '12. Property Binding in the Angular. Bind Property to the attributes in the angular template.',
+    '13. Event Binding in Angular. Handle Click Events in the Angular Explained.',
   ];
   conceptsNeedHandsOn: string[] = [
     'StyleUrls & Styles',
     'Selectors - Attribute, Class Selectors',
     'Data binding - String Interpolation',
+    'Property Binding',
+    'Event Binding',
   ];
   constructor() {}
 
