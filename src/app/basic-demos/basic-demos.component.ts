c64ebc8bb69a4526f565bd957311aa14f0b304ec
harishreddyhrd <hari.000703@gmail.com>
2021-11-10 05:01:51 +0530

    ViewEncapsulation - Demo

--- src/app/basic-demos/basic-demos.component.ts
+++ src/app/basic-demos/basic-demos.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-basic-demos',
   // template: `<p>TEMPLATE Works</p>`,
   templateUrl: './basic-demos.component.html',
   styleUrls: ['./basic-demos.component.scss'],
+  // encapsulation: ViewEncapsulation.Emulated,
+  encapsulation: ViewEncapsulation.None,
 })
 export class BasicDemosComponent implements OnInit {
   toDoVideosList: string[] = [
