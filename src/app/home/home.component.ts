ecc9f6df2650f81660ade283562aa54599ceb28d
harishreddyhrd <hari.000703@gmail.com>
2021-11-12 06:06:53 +0530

    navigate, navigateByUrl - demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  navigateToUsers() {
+    this._router.navigate(['/users']);
   }
-
+  navigateToCategories() {
+    this._router.navigateByUrl('/categories');
+  }
 }
