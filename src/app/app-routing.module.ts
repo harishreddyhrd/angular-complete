0aef396ae364297272a2075869a198ddd6187f3a
harishreddyhrd <hari.000703@gmail.com>
2021-11-12 06:17:01 +0530

    ActivatedRoute - Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UsersComponent } from './users/users.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UsersComponent },
   { path: 'categories', component: CategoriesComponent },
 ];
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  userId!: number;
+  userName!: string;
+  constructor(private _activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.userId = this._activatedRoute.snapshot.params['id'];
+    this.userName = this._activatedRoute.snapshot.params['name'];
   }
-
 }
