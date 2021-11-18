641a0f7895f23c95bc8ee24c7eb7e1c65d6f3a24
harishreddyhrd <hari.000703@gmail.com>
2021-11-18 05:33:19 +0530

    error(), compplete() demos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,18 +26,31 @@ export class HomeComponent implements OnInit {
     //   console.log(count);
     // });
 
-    let customObservable = new Observable((observer) => {
+    let customObservable: Observable<number> = new Observable((observer) => {
       let count = 0;
-      setTimeout(() => {
+      setInterval(() => {
         observer.next(count);
+        if (count > 10) {
+          observer.error('Count > 10');
+        }
+        if (count > 5) {
+          observer.complete();
+        }
         count++;
       }, 1000);
     });
 
-    this.customObservableSubscription = customObservable.subscribe((count) => {
-      // console.log(`theCustomObservable: ${count}`);
-      console.log(count);
-    });
+    this.customObservableSubscription = customObservable.subscribe(
+      (count) => {
+        console.log(`theCustomObservable :: ${count}`);
+      },
+      (error) => {
+        console.log(`theCustomObservable :: ERROR = ${error}`);
+      },
+      () => {
+        console.log(`Observation complete`);
+      }
+    );
   }
 
   navigateToUsers() {
