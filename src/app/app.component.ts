cc7fc79418106c333a2042e91a51f84a9bf059a0
harishreddyhrd <hari.000703@gmail.com>
2021-11-19 12:51:34 +0530

    Basic form generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'inititate';
-  description = 'description';
+  title = 'Basic form';
+  description = 'Boiler plate for forms demo';
 }
