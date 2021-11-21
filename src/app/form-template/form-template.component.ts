a444812594dd1ab8555cb26a825dda90788521bc
harishreddyhrd <hari.000703@gmail.com>
2021-11-21 05:56:18 +0530

    Angular basic FORM BOILERPLATE - UI Updated

--- src/app/form-template/form-template.component.ts
+++ src/app/form-template/form-template.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-template',
   templateUrl: './form-template.component.html',
-  styleUrls: ['./form-template.component.scss']
+  styleUrls: ['./form-template.component.scss'],
 })
 export class FormTemplateComponent implements OnInit {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
