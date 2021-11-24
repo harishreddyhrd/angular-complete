55e184aead57230527ddb60f6253b4097d0e4472
harishreddyhrd <hari.000703@gmail.com>
2021-11-25 05:08:15 +0530

    CustomPipe 'trimString: number of characters' created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'inititate';
-  description = 'description';
+  title = 'Custom Pipe';
+  description = 'Demonstartion of how a custom pipe works';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrimStringPipe } from './trim-string.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrimStringPipe
   ],
   imports: [
     BrowserModule,
