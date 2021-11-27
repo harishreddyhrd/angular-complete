4f4de6369455a42c70666d3cdbe8bb25f4954a7c
harishreddyhrd <hari.000703@gmail.com>
2021-11-27 05:38:25 +0530

    add-movie created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent
   ],
   imports: [
     BrowserModule,
