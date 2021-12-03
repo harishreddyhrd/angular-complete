898f0ed358edf40b0ae938d99cdef9ad2fd31201
harishreddyhrd <hari.000703@gmail.com>
2021-12-03 07:43:29 +0530

    ERROR MESSAGES - updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -42,13 +42,25 @@ export class AuthService {
       }
       return throwError(theErrorMessage);
     }
-    
+
     switch (theError?.error?.error?.message) {
       case 'EMAIL_EXISTS':
-        theErrorMessage = 'Email already exists.';
+        theErrorMessage = `${theError?.error?.error?.message}: The email address is already in use by another account.`;
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        theErrorMessage = `${theError?.error?.error?.message}: Password sign-in is disabled for this project.`;
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        theErrorMessage = `${theError?.error?.error?.message}: We have blocked all requests from this device due to unusual activity. Try again later.`;
         break;
       case 'EMAIL_NOT_FOUND':
-        theErrorMessage = 'Email does not exist.';
+        theErrorMessage = `${theError?.error?.error?.message}: There is no user record corresponding to this identifier. The user may have been deleted`;
+        break;
+      case 'INVALID_PASSWORD':
+        theErrorMessage = `${theError?.error?.error?.message}: The password is invalid or the user does not have a password`;
+        break;
+      case 'USER_DISABLED':
+        theErrorMessage = `${theError?.error?.error?.message}: The user account has been disabled by an administrator`;
         break;
     }
     return throwError(theErrorMessage);
