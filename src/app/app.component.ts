f4d79e13cdbfd3d1a46e3d7cd558aebcb0f80909
harishreddyhrd <hari.000703@gmail.com>
2021-12-04 06:19:17 +0530

    log the user (register / login ) response = log the token data of (registered / logged in) USER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Authentication';
   description = 'Implement authentication in angular';
+
+  constructor(private _authService: AuthService){}
+
+  ngOnInit() {
+    this._authService.user$.subscribe(data => {
+      console.log("USER :: ", data)
+    })
+  }
 }
