4e7e41284f95cd4d8f74a8f0f5e009f0d7598269
harishreddyhrd <hari.000703@gmail.com>
2021-12-04 06:26:18 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserFormComponent } from './user-form/user-form.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'authentication', component: UserFormComponent},
+  {path: 'welcome', component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
