f25ae85f3779334fba0b3172cc9ad824f435bb2e
harishreddyhrd <hari.000703@gmail.com>
2021-12-09 06:07:29 +0530

    .

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,17 +10,32 @@ import { Movie } from '../models/movie';
 export class MovieService {
   URL = `https://angular-complete-d061e-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json`;
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient, private _authService: AuthService) {}
 
   getDataFromFireBase(): Observable<any> {
-    return this._http.get(this.URL);
+    let searchParams = new HttpParams();
+    const userToken = this._authService.user$.value._token;
+    searchParams = searchParams.append('auth', userToken)
+    return this._http.get(this.URL, {
+      params: searchParams,
+    });
   }
 
   submitDataToFireBase(data: Movie): Observable<any> {
-    return this._http.post(this.URL, data);
+    let searchParams = new HttpParams();
+    const userToken = this._authService.user$.value._token;
+    searchParams = searchParams.append('auth', userToken);
+    return this._http.post(this.URL, data, {
+      params: searchParams,
+    });
   }
 
   deleteAllDataFromFireBase() {
-    return this._http.delete(this.URL);
+    let searchParams = new HttpParams();
+    const userToken = this._authService.user$.value._token;
+    searchParams = searchParams.append('auth', userToken);
+    return this._http.delete(this.URL, {
+      params: searchParams,
+    });
   }
 }
